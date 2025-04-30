df234ded4a32e648b2b9f836eabf68dc44bf543f
Jose Murga <murga.jose16@gmail.com>
2025-04-30 05:26:38 -0500

    feat: Header Base with Overlay

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CustomCookieService } from '@core/services';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(private customCookieService: CustomCookieService) { }
 
+  signOut(): void {
+    this.customCookieService.deleteToken();
+  }
 }
