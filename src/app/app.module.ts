3cdc2de4f2ab8acc955544067f942be9083f860d
Jose Murga <murga.jose16@gmail.com>
2025-04-30 04:18:25 -0500

    feat: Http Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,10 +13,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NgbModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient(
+      withFetch()
+    )
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
